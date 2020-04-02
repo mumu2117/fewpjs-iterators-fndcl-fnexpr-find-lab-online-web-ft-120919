377772cb583d2baab4a2b5352440effba658df36
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-04-02 14:10:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+ const testVar = {const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  
+]}
 
-function testFunc() {
+ function testFunc() {
+  function testFunc() {	function superbowlWin (record) {
+    return "hi"	  let result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
   return "hi"
-}
+ }
